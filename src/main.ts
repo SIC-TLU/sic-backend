388faa175d317257c5286f14376b032e33c0a3ba
lovanbang <lovanbangbox9@gmail.com>
2024-09-19 23:42:44 +0700

    feat: create api available admin create user

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const HOST = configService.get<string>('HOST');
   const PORT = configService.get<string>('PORT');
+
+  app.setGlobalPrefix('api/v1', { exclude: ['/'] });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(PORT, () => {
     console.log(`App running in ${HOST}:${PORT}`);
   });
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,11 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { hashPassword } from '@/helpers';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private configService: ConfigService,
+  ) {}
+
+  async isUserExist(username: string) {
+    const hasUser = await this.userModel.exists({ username });
+    if (hasUser) return true;
+
+    return false;
+  }
+
+  async isEmailExist(email: string) {
+    const hasEmail = await this.userModel.exists({ email });
+    if (hasEmail) return true;
+
+    return false;
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const { username, email, password, image } = createUserDto;
+
+    // Check username has exist?
+    const isUserExist = await this.isUserExist(username);
+    if (isUserExist) {
+      throw new BadRequestException(
+        'Username already exists. Please use another username',
+      );
+    }
+
+    // Check email has exist?
+    const isExist = await this.isEmailExist(email);
+    if (isExist) {
+      throw new BadRequestException(
+        `Email ${email} already exists. Please use another email`,
+      );
+    }
+
+    // Hash password
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = await this.userModel.create({
+      username,
+      email,
+      password: hashedPassword,
+      role: this.configService.get<string>('ROLE_USER'),
+      image,
+    });
+
+    return { _id: newUser._id };
   }
 
   findAll() {
@@ -17,6 +70,7 @@ export class UsersService {
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
+    console.log('updateUserDto: ', updateUserDto);
     return `This action updates a #${id} user`;
   }
 
