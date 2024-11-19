3886fd68cb7fbbe14b345f4727a5de93f726cf03
lovanbang <lovanbangbox9@gmail.com>
2024-11-19 11:07:13 +0700

    feat: send code for user via mail

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,16 @@ import { PostModule } from './modules/post/post.module';
 import { JwtAuthGuard } from './auth/passport/jwt-auth.guard';
 import { RolesGuard } from './auth/roles.guard';
 import { User, UserSchema } from './modules/users/schemas/user.schema';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    UsersModule,
+    AuthModule,
+    PostModule,
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -23,9 +28,35 @@ import { User, UserSchema } from './modules/users/schemas/user.schema';
       }),
       inject: [ConfigService],
     }),
-    UsersModule,
-    AuthModule,
-    PostModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          service: 'gmail',
+          host: 'smtp.gmail.com',
+          port: 456,
+          // secure: true,
+          // ignoreTLS: true,
+          // secure: false,
+          auth: {
+            user: configService.get<string>('MAIL_USER'),
+            pass: configService.get<string>('MAIL_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: '"No Reply" <no-reply@localhost>',
+        },
+        // preview: true,
+        template: {
+          dir: process.cwd() + '/src/mail/templates/',
+          adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [
